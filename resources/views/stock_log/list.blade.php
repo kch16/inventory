4be50c0047f51870aa9ac04ec3e6dd9394432cc7
KK <kch6506@gmail.com>
2026-03-03 22:34:24 +0900

    21. Accessor 액세서, 재고관리프로그램, 라라벨12

--- resources/views/stock_log/list.blade.php
+++ resources/views/stock_log/list.blade.php
@@ -21,8 +21,8 @@
                 <tr>
 					<td>{{$log->created_at->format('Y-m-d H:i:s')}}</td>
 					<td>{{($log->product?->name ?? '삭제된 상품')}} ({{$log->product->sku ?? '-'}})</td>
-					<td>{{$log->change_type}}</td>
-					<td>{{$log->change_amount}}</td>
+					<td>{{$log->change_type_label}}</td>
+					<td>{{$log->signed_amount}}</td>
 				</tr>    
                 @endforeach
             </tbody>
